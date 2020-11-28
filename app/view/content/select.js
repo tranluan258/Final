7f1bea8c10bdf89e5bd3d1facad2477ad17e95b1
Tran Luan <tranluanqqq@gmail.com>
2020-11-28 14:51:18 +0700

    select all

---
+++ app/view/content/select.js
@@ -0,0 +1,17 @@
+
+
+function selectAll() {
+    var items = document.getElementsByName('liststudent[]');
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].type == 'checkbox')
+            items[i].checked = true;
+    }
+}
+
+function UnSelectAll() {
+    var items = document.getElementsByName('liststudent[]');
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].type == 'checkbox')
+            items[i].checked = false;
+    }
+}
